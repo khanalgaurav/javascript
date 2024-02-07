f009ab9503a2a57dd2eb0d23bf8f3fbbcc62083e
khanalgaurav <khanalgaurav96@gmail.com>
2024-02-07 22:36:25 +0545

    webhook check

---
+++ Javascript/01_variables.js
@@ -0,0 +1,15 @@
+/*
+Constant is used to permanantly assign value.
+They cannot be changed after assigned once.
+*/
+const piValue=3.14
+
+/*
+let is used to define a variable in js.
+var is also used to define a variable in js but it causes issues in block scope and functional scope.
+"So we use let to define variable". 
+*/
+let firstName="Gaurav"
+var lastName="Khanal"
+// we can also create variables without declaring its type but this is dumb so dont do it.
+addressCity="Kapan"
---
+++ Javascript/02_datatypes.js
@@ -0,0 +1,9 @@
+let name="Gaurav"// this is a string type which can be verified by using typeof.
+console.log(typeof name)
+
+let age = 3 // this is a number which can be of size approx (2 power 53 or something) for more number we use "bigint" datatype
+
+let status= true  // this is a boolean datatype which can be either true or false
+
+//null is a stand alone value which can be assigned to variable
+
