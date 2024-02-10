cca3db0c9b1a4571cc94e17c1f257844fd29d76d
khanalgaurav <khanalgaurav96@gmail.com>
2024-02-10 20:58:44 +0545

    data-types, strings and numbers in js

---
+++ Javascript/03_string.js
@@ -0,0 +1,19 @@
+const name = "Gaurav"
+const age = 21
+
+console.log(`Hi my name is ${name}. I am ${age} years old.`)
+
+
+// another way to declare string 
+
+const userName = new String('Omayera')
+console.log(userName)
+
+// watch about string methods 
+//some examples are:
+    //toUpper
+    //toLower
+    //slice
+    //trim etc.....
+    // fking master it
+    
